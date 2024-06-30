ae43a8d4b0d40705e39ab0653048a6af8e67332c
XinluChen <lucy.chen991207@gmail.com>
2024-06-30 04:31:56 -0700

    OrientCam finish implementation

--- Assets/_Project/_Workspaces/DevTest/Scripts/Runtime/Orientation/OrientationManager.cs
+++ Assets/_Project/_Workspaces/DevTest/Scripts/Runtime/Orientation/OrientationManager.cs
@@ -7,27 +7,18 @@ public class OrientationManager : MonoBehaviour
 
 {
     public static OrientationManager Instance;
+    private Vector3 _pos;
+    private Quaternion _rotateOffset;
+
     private void Awake()
     {
         Instance = this;
-        _rotateOffset = Quaternion.LookRotation(_pos);
-        transform.localRotation = _rotateOffset;
     }
 
 
-    private Vector3 _pos;
-
-    private Quaternion _rotateOffset;
-
-    
-   
-
-    // Update is called once per frame
     public void UpdateOrientation(Quaternion value)
     {
-        
-        transform.localRotation = _rotateOffset * Quaternion.Inverse(value);
-        Debug.DrawLine(transform.parent.position, (this.transform.position), Color.red);
-            //(transform.parent.position, _pos * 1.2f, Color.red);
+        transform.localRotation = Quaternion.Inverse(value);
+        //Debug.DrawLine(transform.parent.position, (this.transform.position), Color.red);
     }
 }
