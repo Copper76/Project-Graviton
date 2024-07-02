1edcf2e3efd9daf3079b10ff19c1a751f5b8ba8c
XinluChen <lucy.chen991207@gmail.com>
2024-07-01 17:40:48 -0700

    make selected UI changed by object size/rotation, invisible unnessary arrow when select 1.

--- Assets/_Project/_Workspaces/DevTest/Scripts/Runtime/UI&Visualization/SelectedUI.cs
+++ Assets/_Project/_Workspaces/DevTest/Scripts/Runtime/UI&Visualization/SelectedUI.cs
@@ -5,29 +5,129 @@ using UnityEngine.UIElements;
 
 public class SelectedUI : MonoBehaviour
 {
+    private enum Direction
+    {
+        PosX,
+        NegX,
+        PosY,
+        NegY,
+        PosZ,
+        NegZ,
+    }
+    private int _selectedDirection;
+    private GameObject _currDirObject;
 
-    [SerializeField] private Transform[] _Dir;
+    [SerializeField] private Transform[] _DirTransform;//in order same as enum Direction,
 
+
+    
+
+    private Vector3[] _OriDirPos;
+
+    private void Awake()
+    {
+        _OriDirPos = new Vector3[_DirTransform.Length];
+        for (int i = 0; i < _OriDirPos.Length; i++)
+        {
+            _OriDirPos[i] = _DirTransform[i].localPosition;
+        }
+    }
+    private void Start()
+    {
+        
+        
+    }
+
+    //Test
+    public void TestDirection()
+    {
+        //TEST
+        StartCoroutine(testIt());
+    }
+    //Test
+    private IEnumerator testIt()
+    {
+        for (int i = 0; i < _DirTransform.Length; i++)
+        {
+            yield return new WaitForSeconds(2f);
+            OnDirectionPicked(_DirTransform[i].gameObject);
+
+            yield return new WaitForSeconds(2f);
+            OnDirectionUnPicked();
+        }
+        
+
+    }
     /// <summary>
     /// Invisible other gizmo direction UI
     /// </summary>
     public void OnDirectionPicked(GameObject dirObject)
     {
-        for (int i = 0; i < _Dir.Length; i++)
+        _currDirObject = dirObject;
+
+        //set unselect axis invisible
+        for (int i = 0; i < _DirTransform.Length; i++)
         {
-            if (_Dir[i] != dirObject) _Dir[i].gameObject.SetActive(false);
+
+            if (_DirTransform[i].gameObject != dirObject)
+            {
+                _DirTransform[i].gameObject.SetActive(false);
+            }
+
+            else 
+            { 
+                _selectedDirection = i; 
+            }
+            
         }
 
         //TODO: move correspondingly by player interaction
 
     }
 
-    public void OnDirectionUnPicked(GameObject dir)
+    public void OnDirectionUnPicked()
     {
-        for (int i = 0; i < _Dir.Length; i++)
+        //Set all the axis visible
+        for (int i = 0; i < _DirTransform.Length; i++)
         {
-            if (_Dir[i] != dir) _Dir[i].gameObject.SetActive(true);
+            if (_DirTransform[i] != _currDirObject) _DirTransform[i].gameObject.SetActive(true);
         }
 
+    }
+
+    public void ResetPos()
+    {
+        //Reset the position first in case it has been resize
+        for (int i = 0; i < _OriDirPos.Length; i++)
+        {
+            _DirTransform[i].localPosition = _OriDirPos[i];
+        }
+    }
+    public void ResizeUI(float[] _boundInfo)
+    {
+        ResetPos();
+
+        Vector3 _newDir;
+        float _offset;
+        for (int i = 0; i < _boundInfo.Length; i++)
+        {
+            _newDir = _DirTransform[i].position;
+
+            if (i % 2 != 0) _offset = -_boundInfo[i] - 0.5f;
+            else _offset = _boundInfo[i] - 0.5f;
+            
+            if (i % 2 != 0)  _offset = -_offset;
+
+
+            if (i<2)//x
+                _newDir.x += _offset;
+            else if (i<4)//y
+                _newDir.y += _offset;
+            else//z
+                _newDir.z += _offset;
+
+            _DirTransform[i].position = _newDir;
+            
+        }
     }
 }
--- Assets/_Project/_Workspaces/DevTest/Scripts/Runtime/UI&Visualization/Visualization.cs
+++ Assets/_Project/_Workspaces/DevTest/Scripts/Runtime/UI&Visualization/Visualization.cs
@@ -13,31 +13,118 @@ using static UnityEngine.GraphicsBuffer;
 public class Visualization : MonoBehaviour
 {
 
+    
     [SerializeField] private GameObject _selectedItem;
     [SerializeField] private GameObject _selectedUIPref;
     
     private GameObject _currUI;
-    
 
-    // Start is called before the first frame update
+    private bool _isSelected;
+    private GameObject _currTarget;
+
+
+
+    //for rescale
+    Bounds _bounds;
+    Renderer _targetRenderer;
+    Vector3 _center;
+    Vector3 _extents;
+    Vector3 _currSize;
+    Quaternion _currRot;
+
+    //TEST
+    [SerializeField] private GameObject[] _selectedItemTest;
+
+    private void Awake()
+    {
+        _isSelected = false;
+    }
+
     void Start()
     {
-
-        //TEST
-        OnSelect(_selectedItem.transform);
-        //Invoke("OnDeSelect", 5f);
         
         _currUI = Instantiate(_selectedUIPref);
         _currUI.SetActive(false);
+
+
+        //TEST
+        StartCoroutine(testIt());
+    }
+
+    private IEnumerator testIt()
+    {
+        
+        for (int i = 0; i < _selectedItemTest.Length; i++)
+        {
+
+            OnSelect(_selectedItemTest[i].transform);
+
+            yield return new WaitForSeconds(25f);
+            OnDeSelect();
+            yield return new WaitForSeconds(5f);
+        }
+
+
+    }
+
+
+
+
+    private void LateUpdate()
+    {
+        if (!(_currUI.activeSelf) || _currTarget == null) return;
+        //update UI position and size
+        _currUI.transform.position = _currTarget.transform.position;
+
+        if (_currSize != _currTarget.transform.localScale || _currRot != _currTarget.transform.rotation)
+        {
+            _currUI.GetComponent<SelectedUI>().ResizeUI(GetTargetBoundInfo());
+            _currSize = _currTarget.transform.localScale;
+            _currRot = _currTarget.transform.rotation;
+        }
+
+
+    }
+
+    private float[] GetTargetBoundInfo()
+    {
+        _targetRenderer = _currTarget.GetComponent<Renderer>();
+        if (_targetRenderer == null) return null;
+
+        _bounds = _targetRenderer.bounds;
+        _extents = _bounds.extents;
+
+        // Calculate distances from the center to each direction
+        float disToPosX = _extents.x;
+        float disToNegX = -_extents.x;
+        float disToPosY = _extents.y;
+        float disToNegY = -_extents.y;
+        float disToPosZ = _extents.z;
+        float disToNegZ = -_extents.z;
+
+
+        return new float[]{disToPosX, disToNegX, disToPosY, disToNegY, disToPosZ, disToNegZ};
     }
 
     public void OnSelect(Transform target)
     {
+        
+        _currTarget = target.gameObject;
+        _currRot = target.rotation;
+        _currSize = target.localScale;
+
         _currUI.SetActive(true);
+        _currUI.GetComponent<SelectedUI>().ResizeUI(GetTargetBoundInfo());
+
+        //TEST
+        _currUI.GetComponent<SelectedUI>().TestDirection();
+
+
     }
 
     public void OnDeSelect()
     {
+        _currTarget = null;
         _currUI.SetActive(false);
     }
 
